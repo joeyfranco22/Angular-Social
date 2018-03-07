a8c40c1245702bce7a4f93143b76d517c81624d8
Ginel Exaltado <ginelexaltado@gmail.com>
2018-03-07 16:41:21 +0800

    authentication commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyB_N7O29_CjQZd8fPoTDWMJQQlurtixLew",
+    authDomain: "angular-social-17e16.firebaseapp.com",
+    databaseURL: "https://angular-social-17e16.firebaseio.com",
+    projectId: "angular-social-17e16",
+    storageBucket: "",
+    messagingSenderId: "711760765053"
     }
 };
